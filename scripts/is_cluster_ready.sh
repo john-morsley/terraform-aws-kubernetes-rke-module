2384c98b5264fbfcffe536e507cf8b0300307812
John Morsley <johnmorsley@gmail.com>
2020-05-20 10:16:35 +0100

    Refactoring

--- scripts/is_cluster_ready.sh
+++ scripts/is_cluster_ready.sh
@@ -1,19 +1,13 @@
 ﻿#!/usr/bin/env sh
 
-echo "-------------------------------------------------------------------------------"
-
 echo "CURRENT: ${CURRENT}"
 echo "ROOT: ${ROOT}"
-
-echo "-------------------------------------------------------------------------------"
 
 KUBE_CONFIG_FOLDER="${CURRENT}/K8s"
 echo "KUBE_CONFIG_FOLDER: ${KUBE_CONFIG_FOLDER}"
 
 COMMON_KUBERNETES="${CURRENT}/${ROOT}/../common-kubernetes"
 echo "COMMON_KUBERNETES: ${COMMON_KUBERNETES}"
-
-echo "-------------------------------------------------------------------------------"
 
 bash ${COMMON_KUBERNETES}/scripts/is_cluster_ready.sh ${KUBE_CONFIG_FOLDER}
 

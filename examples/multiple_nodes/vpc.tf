18e897325d3cd35ce8328b037b92961e3ce6b390
John Morsley <johnmorsley@gmail.com>
2020-05-21 11:09:21 +0100

    Refactoring

--- examples/multiple_nodes/vpc.tf
+++ examples/multiple_nodes/vpc.tf
@@ -13,7 +13,6 @@ module "vpc" {
   
   vpc_cidr = var.vpc_cidr
   public_subnet_cidrs = var.public_subnet_cidrs
-  #private_subnet_cidrs = var.private_subnet_cidrs
 
   public_subnet_tags = local.cluster_id_tag
 

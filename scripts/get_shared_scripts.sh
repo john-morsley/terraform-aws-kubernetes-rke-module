722e526edfa8e13d3a111fdf52f1cd5a21c14fee
John Morsley <johnmorsley@gmail.com>
2020-05-21 12:49:21 +0100

    Bug is get shared scripts

--- scripts/get_shared_scripts.sh
+++ scripts/get_shared_scripts.sh
@@ -4,6 +4,7 @@ echo 'Cloning Shared Scripts repository...'
 
 echo "SHARED_SCRIPTS_FOLDER: ${SHARED_SCRIPTS_FOLDER}"
 
-git clone https://github.com/john-morsley/shared-scripts.git
+git clone https://github.com/john-morsley/shared-scripts.git \
+  ${SHARED_SCRIPTS_FOLDER}
 
 exit 0
